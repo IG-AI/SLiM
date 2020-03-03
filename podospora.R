5d5e463ccc97474ffae18b4a8f1b2305b66b43b6
Daniel Ågstrand <d.e.agstrand@gmail.com>
2020-03-03 17:22:36 +0100

    Implemented correct fitness function and modifyChild

--- podospora.R
+++ podospora.R
@@ -4,15 +4,17 @@ V1Vintercation = 60
 individuals = 1000
 invaders = 10
 selfing = 0.9
+pnormsd = 0.1
+pnormmaxfitness = 1
 
-system(paste0("./slim -d V1Vintercation=", V1Vintercation, " -d individuals=", individuals, " -d invaders=", invaders, " -d selfing=", selfing, " podospora.slim"))
+system(paste0("./slim -d V1Vintercation=", V1Vintercation, " -d individuals=", individuals, " -d invaders=", invaders, " -d selfing=", selfing, " -d pnormsd=", pnormsd, " -d pnormmaxfitness=", pnormmaxfitness, " podospora.slim"))
 SLiMdata <- read.delim("mutCount.txt", header = TRUE, sep = ",", dec = ".")
 
 generations <- rownames(SLiMdata)
 colorList <- c("black", "red", "blue", "purple")
 for (i in 1:length(colorList)) {
   if (i == 1) {
-    plot(generations, SLiMdata[,i], col=colorList[i] , type="l" , lwd=3 , xlab="Generations" , ylab="Mutation Frequency", main="Mutation changes")
+    plot(generations, SLiMdata[,i], col=colorList[i] , type="l" , lwd=3 , xlab="Generations" , ylab="Mutation Frequency", main="Mutation changes", ylim=c(0,1))
   }
   else {
     lines(generations, SLiMdata[,i], col=colorList[i], type="l" , lwd=3)
